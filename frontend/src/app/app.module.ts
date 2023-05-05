1ee8e58b975c80ee4bc23135a6e04dc61c00c4b9
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2023-05-05 19:53:32 -0300

    Mueve AppRoutingModule al final por una cuestion de accesibilidad

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,15 +6,9 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    SharedModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SharedModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
