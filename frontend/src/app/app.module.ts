2148c56d0b98b9b14d8f00435c436d4b5991589e
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2023-05-05 20:33:39 -0300

    Crea modulo Home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, SharedModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, SharedModule, AppRoutingModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
