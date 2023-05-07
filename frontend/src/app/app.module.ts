9d25d5bcee66d406ef5f3e21962187b7dd6deb1f
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2023-05-06 21:24:24 -0300

    Agrega modulo auth con sus respectivos componentes: login - register

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HomeModule } from './home/home.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { HomeModule } from './home/home.module';
     SharedModule,
     HomeModule,
     AppRoutingModule,
+    AuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
