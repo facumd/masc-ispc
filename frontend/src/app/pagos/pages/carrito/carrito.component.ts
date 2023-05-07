cb0266cbb9e49db0d8cbc5dcb7defa35e6166c0c
Marialer27 <maria.alerodriguez@gmail.com>
2023-05-07 11:05:00 -0300

    Agrego el componente carrito y su respectivo css

--- frontend/src/app/pagos/pages/carrito/carrito.component.ts
+++ frontend/src/app/pagos/pages/carrito/carrito.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-carrito',
   templateUrl: './carrito.component.html',
+  styleUrls: ['./carrito.component.css']
 })
 export class CarritoComponent {
   constructor() {}
