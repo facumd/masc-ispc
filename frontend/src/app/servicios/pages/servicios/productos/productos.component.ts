4912f9a67673214ea3fe98901aaf9be49a4a94d6
TamiSorsaburu <tamysorsaburu@gmail.com>
2023-05-20 00:22:39 -0300

    Agrego servicios y los  inyecto a los componentes

--- frontend/src/app/servicios/pages/servicios/productos/productos.component.ts
+++ frontend/src/app/servicios/pages/servicios/productos/productos.component.ts
@@ -1,17 +1,24 @@
-import { Component, Input } from '@angular/core';
-import { Productos } from 'src/app/servicios/models/productos.model';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from '../datos.service';
+
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
 })
-export class ProductosComponent {
-  /* @Input() productos: Productos = {
-    id: 0,
-    title: '',
-    price: 0,
-    images: [],
-  }; */
+export class ProductosComponent implements OnInit{
+  productos : any = [];
+  constructor(private datos:DatosService) {
+    this.datos.getDatos().subscribe({
+        next: (productosData) => {
+            this.productos= productosData
+        },
+        error: (errorData) => {
+            console.error(errorData);
+        }
+    });
+   }
 
-  @Input() productos!: Productos;
+  ngOnInit(): void {
+  }
 }
