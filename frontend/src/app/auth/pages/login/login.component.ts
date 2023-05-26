9d06dc0cd6427826de3a3846ca890a267bbaff19
Stefanía <99340983+stefaniajara@users.noreply.github.com>
2023-05-26 17:55:51 -0300

    ReactiveForms Login/SignIn
    Se agregan Formularios Reactivos a Login y Registro, con sus respectivas importaciones en los módulos

--- frontend/src/app/auth/pages/login/login.component.ts
+++ frontend/src/app/auth/pages/login/login.component.ts
@@ -1,7 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  
+  loginForm = this.formBuilder.group({
+    email:['', [Validators.required, Validators.email]],
+    password:  ['', [Validators.required, Validators.minLength(5)]],
+  })
+
+  constructor(private formBuilder: FormBuilder, private router:Router) { }
+
+  ngOnInit(): void {}
+
+  get email(){
+    return this.loginForm.controls.email;
+  }
+
+  get password(){
+    return this.loginForm.controls.password;
+  }
+
+  login() {
+    if(this.loginForm.valid) {
+      console.log ("Iniciar Sesión");
+      this.router.navigateByUrl('/');
+      this.loginForm.reset();
+    }
+    else {
+      this.loginForm.markAllAsTouched();
+      alert("Para iniciar sesión debe completar todos los campos");
+    }
+  }
+
+}
