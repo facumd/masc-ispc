7735e651e4be4dbf98874bd357a2c5f39724d14d
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2023-06-05 00:50:18 -0300

    Trae productos de bd y actualiza app moduel

--- frontend/src/app/servicios/pages/servicios/productos/productos.component.ts
+++ frontend/src/app/servicios/pages/servicios/productos/productos.component.ts
@@ -1,24 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { DatosService } from '../datos.service';
-
+import { Producto } from '../interface/productos.interface';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
 })
-export class ProductosComponent implements OnInit{
-  productos : any = [];
-  constructor(private datos:DatosService) {
-    this.datos.getDatos().subscribe({
-        next: (productosData) => {
-            this.productos= productosData
-        },
-        error: (errorData) => {
-            console.error(errorData);
-        }
-    });
-   }
+export class ProductosComponent implements OnInit {
+  products: any[] = [];
 
-  ngOnInit(): void {
+  constructor(private datos: DatosService) {
+    this.datos.getDatos().subscribe({
+      next: (response) => {
+        this.products = response;
+        console.log(this.products);
+      },
+      error: (errorData) => {
+        console.error(errorData);
+      },
+    });
   }
+
+  ngOnInit(): void {}
 }
