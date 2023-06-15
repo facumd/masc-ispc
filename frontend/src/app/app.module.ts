7cbf5ed500ae151533952b7b986f0e7a6d594c62
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2023-06-15 00:49:27 -0300

    Sienta bases para rebrand de la pagina

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,28 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { HomeModule } from './home/home.module';
-import { AuthModule } from './auth/auth.module';
-import { ServiciosModule } from './servicios/servicios.module';
-import { PagosModule } from './pagos/pagos.module';
-import { AccountModule } from './account/account.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    SharedModule,
-    HomeModule,
-    ServiciosModule,
-    AuthModule,
-    PagosModule,
-    AccountModule,
-    AppRoutingModule,
-  ],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
