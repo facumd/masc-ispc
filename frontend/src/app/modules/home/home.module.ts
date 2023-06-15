ebdbb67acddd2fe2b502a0126441b5f4f4e051fa
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2023-06-15 01:57:49 -0300

    Agrea pagina de contacto y nosotros

--- frontend/src/app/modules/home/home.module.ts
+++ frontend/src/app/modules/home/home.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './pages/home/home.component';
 import { HomeRoutingModule } from './home-routing.moduke';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, AboutUsComponent, ContactComponent],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
