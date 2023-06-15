60e6c3c0fa5c770255295d9b626e3f96af739bb2
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2023-06-15 02:12:19 -0300

    Agrega ruteo para auth y su respectivos contenido en modulos y componentes
    Ahora se consumen los servicios de auth

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,22 @@ import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 import { HomeRoutingModule } from './modules/home/home-routing.moduke';
-
+import { AuthRoutingModule } from './modules/auth/auth-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, SharedModule, HomeRoutingModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    SharedModule,
+    HomeRoutingModule,
+    AuthModule,
+    AuthRoutingModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
