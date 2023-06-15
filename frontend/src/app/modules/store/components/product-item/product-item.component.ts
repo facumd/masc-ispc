c846a745802ad8920421ffa7d8429e8416bb934f
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2023-06-15 02:22:27 -0300

    Modifica componentes detalle e itme de producto
    Ahora consumen los servicios necesarios

--- frontend/src/app/modules/store/components/product-item/product-item.component.ts
+++ frontend/src/app/modules/store/components/product-item/product-item.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.css']
+  styles: [],
 })
 export class ProductItemComponent {
+  @Input() product?: Product;
 
+  constructor(private router: Router) {}
+
+  viewProductDetail(slug: string) {
+    this.router.navigate(['/product', slug]);
+  }
 }
--- frontend/src/app/modules/store/components/product-list/product-list.component.ts
+++ frontend/src/app/modules/store/components/product-list/product-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ProductService } from 'src/app/services/product.service';
+
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styles: [],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  public products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService
+      .getProducts()
+      .subscribe((products) => (this.products = products));
+  }
 }
