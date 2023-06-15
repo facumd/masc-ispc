2c3c4b142a177e8ca26c38ffc8ce440a4497755b
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2023-06-15 19:35:37 -0300

    Actualiza mensaje de error

--- frontend/src/app/modules/store/store-routing.module.ts
+++ frontend/src/app/modules/store/store-routing.module.ts
@@ -4,12 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { StoreComponent } from './pages/store/store.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { NotFoundComponent } from 'src/app/shared/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'store', component: StoreComponent },
   { path: 'product/:slug', component: ProductDetailComponent },
   { path: 'cart', component: CartComponent },
-  // { path: '**', component: NotFoundComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [NavbarComponent, FooterComponent],
+  declarations: [NavbarComponent, FooterComponent, NotFoundComponent],
   exports: [NavbarComponent, FooterComponent],
   imports: [CommonModule, RouterModule],
 })
