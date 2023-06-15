47a0f53a77379e0cc2ad99d23e270bda0ee39747
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2023-06-15 19:43:46 -0300

    Agrega componente de acceso restringido para restringir acceso a usuarios no admin a ciertas rutas

--- frontend/src/app/modules/store/store-routing.module.ts
+++ frontend/src/app/modules/store/store-routing.module.ts
@@ -5,11 +5,13 @@ import { StoreComponent } from './pages/store/store.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { NotFoundComponent } from 'src/app/shared/not-found/not-found.component';
+import { AccessDeniedComponent } from 'src/app/shared/access-denied/access-denied.component';
 
 const routes: Routes = [
   { path: 'store', component: StoreComponent },
   { path: 'product/:slug', component: ProductDetailComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'access-denied', component: AccessDeniedComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
