afba813f1f95e44ec184ceac383c3ddf4cc16b9c
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2023-06-16 15:50:11 -0300

    Corrige detalles en el backend

--- backend/utils/model_abstracts.py
+++ backend/utils/model_abstracts.py
@@ -3,7 +3,8 @@ from django.db import models
 
 
 class Model(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          editable=False, unique=True, max_length=36)
 
     class Meta:
         abstract = True
